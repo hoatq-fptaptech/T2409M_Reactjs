17f340229f21f5858056fefeb3a910c6304f5786
quanghoatrinh <quanghoa.trinh@gmail.com>
2024-12-02 09:45:34 +0700

    link

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function Header(){
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
@@ -9,13 +11,13 @@ export default function Header(){
                 <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
                     <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">Home</a>
+                        <Link to="/" className="nav-link">Home</Link>
                     </li>
                     <li className="nav-item">
-                    <a className="nav-link" href="#">Features</a>
+                    <Link to="/features" className="nav-link">Features</Link>
                     </li>
                     <li className="nav-item">
-                    <a className="nav-link" href="#">Pricing</a>
+                    <Link to="/pricing" className="nav-link">Pricing</Link>
                     </li>
                     <li className="nav-item">
                     <a className="nav-link disabled" aria-disabled="true">Disabled</a>
